5a265e593fc86aee830f02b235552a7421aea37e
Ageu <Borzhesh@yandex.ru>
2025-07-29 23:42:49 +0300

    исправил исключения

--- main-service/src/main/java/ru/practicum/exception/ErrorHandler.java
+++ main-service/src/main/java/ru/practicum/exception/ErrorHandler.java
@@ -4,11 +4,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @Slf4j
-@RestControllerAdvice(basePackages = "ru.practicum")
+@RestControllerAdvice
 public class ErrorHandler {
 
     @ExceptionHandler(ValidationException.class)
@@ -17,21 +16,13 @@ public class ErrorHandler {
     }
 
     @ExceptionHandler(NotFoundException.class)
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    public ErrorResponse handleNotFoundException(NotFoundException e) {
-        log.error("NotFoundException: {}", e.getMessage());
-        return new ErrorResponse("Not found", e.getMessage());
+    public ResponseEntity<ErrorResponse> handlerNotFoundException(NotFoundException e) {
+        log.error("NotFoundException handled: {}", e.getMessage());
+        return new ResponseEntity<>(new ErrorResponse("Not found exception", e.getMessage()), HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(ConflictException.class)
     public ResponseEntity<ErrorResponse> handlerConflictException(ConflictException e) {
         return new ResponseEntity<>(new ErrorResponse("Conflict exception", e.getMessage()), HttpStatus.CONFLICT);
-    }
-
-    @ExceptionHandler
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public ErrorResponse handleOtherExceptions(Exception e) {
-        log.error("Internal error", e);
-        return new ErrorResponse("Internal error", e.getMessage());
     }
 }
